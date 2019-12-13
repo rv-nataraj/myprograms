0ffc46976267aa5e3deca95bf478dda129be49dc
rv-nataraj <rv.nataraj@bitsathy.ac.in>
2019-12-13 17:24:32 +0530

    prog80

---
+++ prog80.py
@@ -0,0 +1,9 @@
+# inputfile.txt
+# apple is the best fruit
+# apple computer is the best computer
+# an apple a day keeps doctor away
+f = open("inputfile.txt", "r")
+print(f.read(8));
+print(f.readline())
+print(f.readline())
+print(f.read());

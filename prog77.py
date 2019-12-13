88f8eec1269c2bea484a6d057deb20a829469e7c
rv-nataraj <rv.nataraj@bitsathy.ac.in>
2019-12-13 14:52:21 +0530

    prog77v2

--- prog77.py
+++ prog77.py
@@ -1,9 +1,22 @@
 
-def pypart(n):
+
+
+def print1(n):
     for i in range(0, n):
         for j in range(0, i+1):
             print("* ",end="")
         print("\r")
 
+def print2(n):
+    k = 2*n - 2
+    for i in range(0, n):
+        for j in range(0, k):
+            print(end=" ")
+        k = k - 2
+        for j in range(0, i+1):
+            print("* ", end="")
+        print("\r")
+
 n = 5
-pypart(n)
+print1(n)
+print2(n)

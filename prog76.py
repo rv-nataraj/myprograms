0e2470f6964ba5cb9e38b27fbb7d5d72a97309d2
rv-nataraj <rv.nataraj@bitsathy.ac.in>
2019-12-13 11:50:23 +0530

    prog76v2

--- prog76.py
+++ prog76.py
@@ -4,7 +4,14 @@ def print_numbers(a,d,n):
 		print(x)
 		x = x + d
 
+def print_numbers2(a,d,n):
+	x=a
+	for i in range(1,n+1):
+		print(x)
+		x = x * d
+
 a = 2
 d = 6
 n = 5
 print_numbers(a, d, n)
+print_numbers2(a, d, n)
